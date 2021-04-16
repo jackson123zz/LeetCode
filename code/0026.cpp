a153f92dfe62982ebde2dc239e6a73b50995f246
jackson123zz <jackson123zz@163.com>
2021-04-16 09:02:00 +0800

    add 0026/35

---
+++ code/0026.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (nums.size() < 2) {
+            return nums.size();
+        }
+        int j = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[j] != nums[i]) {
+                j++;
+                nums[j] = nums[i];
+            }
+        }
+        return j + 1;
+    }
+};
+
+int main()
+{
+    vector<int> nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+    Solution A;
+    int len = A.removeDuplicates(nums);
+    std::cout << "len = " << len << endl;
+    for (int i = 0; i < len; i++) {
+        cout << nums[i];
+    }
+    cout << endl;
+}
---
+++ code/0035.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size(); i++) {
+            if (target == nums[i]) {
+                return i;
+            } else if (target < nums[i]) {
+                nums.insert(nums.begin() + i, target);
+                return i;
+            }
+        }
+        nums.push_back(target);
+        return nums.size() - 1;
+    }
+};
+
+int main()
+{
+    vector<int> nums = { 1,3,5,6 };
+    int target = 0;
+    Solution A;
+    int result = A.searchInsert(nums, target);
+    cout << "resutl = " << result << endl;
+}
